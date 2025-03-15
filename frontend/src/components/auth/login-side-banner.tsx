7abb25cb0a62329bf4033c6400495a0f5c341a34
Zitian Qin <zitianqin2015@gmail.com>
2025-03-15 15:21:26 +1100

    Add jsonwebtoken types

---
+++ frontend/src/components/auth/login-side-banner.tsx
@@ -0,0 +1,7 @@
+export function LoginSideBanner() {
+  return (
+    <div className="bg-red-500 w-full h-full flex items-center justify-center">
+      <p className="text-white text-2xl font-bold">Placeholder</p>
+    </div>
+  )
+}
