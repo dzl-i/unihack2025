42b4709798fc135118235cf3a150ba51aafd61ce
Adrian Balbalosa <87012111+adrianbalbs@users.noreply.github.com>
2025-03-15 11:53:22 +1100

    add some explanation comments to uploadData

--- backend/src/project/uploadData.ts
+++ backend/src/project/uploadData.ts
@@ -11,6 +11,7 @@ export async function projectUploadDataSource(userId: string, projectId: string,
   const userIds = project.users.map((user) => user.userId);
   if (!userIds.includes(userId)) throw { status: 403, message: "You do not have access to this project." };
 
+  // we can just store the key (path) to the file, S3client automatically handles the url stuff
   const key = "input/" + fileName;
 
   const putCommand = new PutObjectCommand({
