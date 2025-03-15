beec872f4e1485bcc8e8cb0da28c4b9ee9ada336
dzl-i <denzel.iskandar@gmail.com>
2025-03-15 23:11:38 +1100

    feat: retrieve data source url

---
+++ backend/src/project/getData.ts
@@ -0,0 +1,12 @@
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import { GetObjectCommand } from "@aws-sdk/client-s3";
+import { s3Client, bucketName } from "../helper/s3Client";
+import { getDataSourceById } from "../helper/dataHelper";
+
+export async function projectGetData(dataSourceId: string) {
+  const dataSource = await getDataSourceById(dataSourceId);
+  if (dataSource === null) throw { status: 404, message: "No data source found." };
+
+  const command = new GetObjectCommand({ Bucket: bucketName, Key: `${dataSource.projectId}/${dataSource.name}` });
+  return getSignedUrl(s3Client, command, { expiresIn: 3600 });
+}
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -34,6 +34,7 @@ import { projectJoin } from './project/join';
 import { projectSendMessage } from './project/send';
 import { projectUploadDataSource } from './project/uploadData';
 import { projectDeleteDataSource } from './project/deleteData';
+import { projectGetData } from './project/getData';
 
 
 interface MulterRequest extends Request {
@@ -245,6 +246,20 @@ app.post('/project/:id/send', authenticateToken, async (req: Request, res: Respo
     const message = await projectSendMessage(userId, projectId, content);
 
     res.status(200).json(message);
+  } catch (error: any) {
+    console.error(error);
+    res.status(error.status || 500).json({ error: error.message || "An error occurred." });
+  }
+});
+
+// Get a file from a project
+app.get('/project/data/:id', authenticateToken, async (req: Request, res: Response) => {
+  try {
+    const dataId = req.params.id;
+
+    const url = await projectGetData(dataId);
+
+    res.status(200).json(url);
   } catch (error: any) {
     console.error(error);
     res.status(error.status || 500).json({ error: error.message || "An error occurred." });
