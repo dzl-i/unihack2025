873ab4d795f2c0cb921736e339b3c95ae146713a
Adrian Balbalosa <87012111+adrianbalbs@users.noreply.github.com>
2025-03-16 01:11:19 +1100

    add langflow fetch to send

--- backend/src/project/send.ts
+++ backend/src/project/send.ts
@@ -14,7 +14,44 @@ export async function projectSendMessage(userId: string, projectId: string, cont
   const message = await sendMessage(userId, projectId, content);
   if (message === null) throw { status: 400, message: "Failed to send message." };
 
-  // TODO: Call to Langflow API
+  const requestBody = {
+    input_value: content,
+    output_type: "chat",
+    input_type: "chat",
+    tweaks: {
+      "ChatInput-TcBLb": {},
+      "ParseData-0oepE": {},
+      "Prompt-s82WS": {},
+      "ChatOutput-KYRZx": {},
+      "OpenAIEmbeddings-cnHCz": {},
+      "OpenAIModel-TTn3d": {},
+      "AstraDB-lvoxd": {
+        // TODO: Comment this out later, need to query metadata.metadata.data_source.record_locator.metadata.projectid key and give the projectid key
+        //"advanced_search_filter": "{\"projectId\":\"<YOUR_ID_HERE>\"}"
+      },
+    },
+  };
 
-  return message; // TODO: Add the response from Langflow API
+  const response = await fetch(process.env.LANGFLOW_URL!, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${process.env.LANGFLOW_KEY!}`,
+    },
+    body: JSON.stringify(requestBody),
+  });
+
+  if (!response.ok) {
+    return { status: 400, message: "Failed to retrieve a response" };
+  }
+
+  const data: any = await response.json();
+
+  // we ball
+  const output = data.outputs?.[0]?.outputs?.[0]?.results?.message?.data?.text;
+  if (!output) {
+    return { status: 400, message: "Failed to retrieve a response" };
+  }
+
+  return message
 }
