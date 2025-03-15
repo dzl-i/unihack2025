6931b6d457803461848bdb27aa0878a5ce6cdc2e
Adrian Balbalosa <87012111+adrianbalbs@users.noreply.github.com>
2025-03-15 11:51:36 +1100

    style fixes for s3Client

--- backend/src/helper/s3Client.ts
+++ backend/src/helper/s3Client.ts
@@ -1,11 +1,11 @@
 import { S3Client } from "@aws-sdk/client-s3";
 
 const s3Client = new S3Client({
-    region: process.env.AWS_REGION!,
-    credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY!,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
-    }
+  region: process.env.AWS_REGION!,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY!,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+  }
 })
 
 export default s3Client
