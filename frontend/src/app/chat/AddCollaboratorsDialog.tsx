d05e72abd385553a3ff282458a9ffac85b717d88
dzl-i <denzel.iskandar@gmail.com>
2025-03-16 16:01:27 +1100

    fix: more eslint issues

--- frontend/src/app/chat/AddCollaboratorsDialog.tsx
+++ frontend/src/app/chat/AddCollaboratorsDialog.tsx
@@ -11,7 +11,6 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
-import { UserPlus } from "lucide-react";
 import AddEmailsInput from "./AddEmailsInput";
 import { useState } from "react";
 
