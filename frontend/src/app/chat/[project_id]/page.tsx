9c858a68249f36714d84497e631fd6ca022fd43d
Merry Rosalie <merryrosalie001@gmail.com>
2025-03-16 14:54:15 +1100

    ending it all

--- frontend/src/app/chat/[project_id]/page.tsx
+++ frontend/src/app/chat/[project_id]/page.tsx
@@ -17,6 +17,8 @@ import { useParams, useRouter } from "next/navigation";
 import { toast } from "sonner";
 import ChatWidget from "../ChatWidget";
 import { DataSource } from "../DataSourcesList";
+import { Cosmograph } from "@cosmograph/react";
+import Graph from "../Graph";
 
 export default function Page() {
   const router = useRouter();
@@ -30,6 +32,9 @@ export default function Page() {
   const [selectedFile, setSelectedFile] = useState<DataSource | undefined>(
     undefined
   );
+
+  // Handle chat/graph view
+  const [isChatView, setIsChatView] = useState(true);
 
   // TODO: Go back to landing page
   if (error) {
@@ -54,18 +59,37 @@ export default function Page() {
         </Suspense>
       </ShadcnSidebar>
       <SidebarInset>
-        <header className="flex h-16 shrink-0 justify-between items-center gap-2 pr-4 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
-          <div className="flex items-center gap-2 px-4">
-            <SidebarTrigger className="-ml-1 size-8" />
-            <Separator orientation="vertical" />
-            <h1>Chat with CollabAI</h1>
-          </div>
-          <Button variant="outline">
-            <TrendingUp />
-            Graph View
-          </Button>
-        </header>
-        <ChatWidget project={project} />
+        {isChatView ? (
+          <>
+            <header className="flex h-16 shrink-0 justify-between items-center gap-2 pr-4 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+              <div className="flex items-center gap-2 px-4">
+                <SidebarTrigger className="-ml-1 size-8" />
+                <Separator orientation="vertical" />
+                <h1>Chat with CollabAI</h1>
+              </div>
+              <Button variant="outline" onClick={() => setIsChatView(false)}>
+                <TrendingUp />
+                Graph View
+              </Button>
+            </header>
+            <ChatWidget project={project} />
+          </>
+        ) : (
+          <>
+            <header className="flex h-16 shrink-0 justify-between items-center gap-2 pr-4 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+              <div className="flex items-center gap-2 px-4">
+                <SidebarTrigger className="-ml-1 size-8" />
+                <Separator orientation="vertical" />
+                <h1>Graph View</h1>
+              </div>
+              <Button variant="outline" onClick={() => setIsChatView(true)}>
+                <TrendingUp />
+                Chat View
+              </Button>
+            </header>
+            <Graph projectId={projectId} />
+          </>
+        )}
       </SidebarInset>
       {selectedFile && (
         <DocumentViewerSidebar
